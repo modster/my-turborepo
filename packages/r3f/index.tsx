fd4bef77aec0258f581b3140ea263c06a9c0798c
E. M. Greeff <unmodster@gmail.com>
2022-11-03 04:01:59 -0600

    installed react-three-next

---
+++ packages/r3f/index.tsx
@@ -0,0 +1,9 @@
+import { useRef, forwardRef } from 'react'
+import { mergeRefs } from 'react-merge-refs'
+
+export * from "./canvas/Blob";
+export * from "./canvas/Logo";
+export * from "./canvas/Scene";
+export * from "./dom/Layout";
+export * from "./dom/Instructions";
+
